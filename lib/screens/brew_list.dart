3bb5df67709cef9b6a1c5ea1868fd0de43a4ba1e
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-08-21 12:12:52 +0100

    screen where all brew are list from the database

---
+++ lib/screens/brew_list.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tut_firebase/models/brew.dart';
+import 'package:tut_firebase/widgets/brew_tile.dart';
+
+class BrewList extends StatefulWidget {
+  @override
+  _BrewListState createState() => _BrewListState();
+}
+
+class _BrewListState extends State<BrewList> {
+  @override
+  Widget build(BuildContext context) {
+    final brews = Provider.of<List<BrewModel>>(context);
+    return brews == null
+        ? Center(
+            child: CircularProgressIndicator(),
+          )
+        : ListView.builder(
+            primary: false,
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: brews.length,
+            itemBuilder: (_, index) {
+              return BrewTile(brew: brews[index]);
+            },
+          );
+  }
+}
