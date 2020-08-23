b15e0e4ca9bd4056f5947bbdf7e1a7857505e1ce
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-08-23 20:31:12 +0100

    helper to check if user is in or out

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tut_firebase/auth/authenticate.dart';
+import 'package:tut_firebase/models/user.dart';
+import 'package:tut_firebase/screens/home.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<UserModel>(context);
     //return either home or authencate widget
-    return Authenticate();
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
   }
 }
