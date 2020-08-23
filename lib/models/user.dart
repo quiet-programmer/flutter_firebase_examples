2c98829f74bf072d57fa07f0044e26be3bf5e1a5
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-08-23 20:33:11 +0100

    added model for user data

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -5,3 +5,17 @@ class UserModel {
     this.uid,
   });
 }
+
+class UserDataModel {
+  final String uid;
+  final String name;
+  final String sugars;
+  final int strength;
+
+  UserDataModel({
+    this.uid,
+    this.name,
+    this.sugars,
+    this.strength,
+  });
+}
