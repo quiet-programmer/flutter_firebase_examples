08c8789ba2460a057a122bcf6529ef511a0deea9
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-08-23 20:29:29 +0100

    added listener for stream provider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tut_firebase/models/user.dart';
 import 'package:tut_firebase/screens/wrapper.dart';
+import 'package:tut_firebase/services/auth.dart';
 
 class App extends StatefulWidget {
   @override
@@ -9,14 +12,21 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Firebase Tutorial",
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          elevation: 0.0,
+    return MultiProvider(
+      providers: [
+        StreamProvider<UserModel>.value(
+          value: AuthService().user,
+        )
+      ],
+      child: MaterialApp(
+        title: "Firebase Tutorial",
+        theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            elevation: 0.0,
+          ),
         ),
+        home: Wrapper(),
       ),
-      home: Wrapper(),
     );
   }
 }
